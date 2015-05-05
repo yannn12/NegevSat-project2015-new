747e2d052ce53816165af4fd10761e9bbfbb245f
yannn12 <yannn12@gmail.com>
2015-05-05 18:45:10 +0300

    spin control module

---
+++ hardware/SpinControlModule.cpp
@@ -0,0 +1,18 @@
+/*
+ * SpinControlModule.cpp
+ *
+ *  Created on: 4 במאי 2015
+ *      Author: Yaniv
+ */
+
+#include <hardware/SpinControlModule.hpp>
+
+SpinControlModule::SpinControlModule() :HWModule("SpinControl"){
+	// TODO Auto-generated constructor stub
+
+}
+
+SpinControlModule::~SpinControlModule() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ hardware/SpinControlModule.hpp
@@ -0,0 +1,19 @@
+/*
+ * SpinrateModule.hpp
+ *
+ *  Created on: 4 במאי 2015
+ *      Author: Yaniv
+ */
+
+#ifndef SPINRATEMODULE_HPP_
+#define SPINRATEMODULE_HPP_
+
+#include "HWModule.hpp"
+
+class SpinControlModule: HWModule {
+public:
+	SpinControlModule();
+	virtual ~SpinControlModule();
+};
+
+#endif /* SPINRATEMODULE_HPP_ */
